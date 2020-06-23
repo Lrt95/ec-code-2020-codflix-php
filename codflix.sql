b7ad949a74542cc646f808681098c91c5d4d1c32
Lrt95 <antony.correia@gmail.com>
2020-06-23 10:48:05 +0200

    create DB Codflix

--- codflix.sql
+++ codflix.sql
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 -- Database: `codflix`
 --
 
+DROP DATABASE IF EXISTS codflix;
+CREATE DATABASE codflix;
 -- --------------------------------------------------------
 
 --
